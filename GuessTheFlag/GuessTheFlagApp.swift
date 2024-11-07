0650551df705d98a7d468e1599f1d7cf6b7ecd58
Jasper Tan <50438928+GitJasperTan@users.noreply.github.com>
2024-11-07 08:30:03 -0800

    initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jasper Tan on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
